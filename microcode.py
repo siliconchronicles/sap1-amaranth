d4ad50c7423854b827ea880d101c0d9f42bfd96e
Daniel F Moisset <dfmoisset@gmail.com>
2024-02-29 00:29:25 +0000

    Add non-HDL module to describe the BE-8 microcode
    Currently, only a few instructions implemented. enough for the demo

---
+++ microcode.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+import enum
+from typing import Literal, TypeAlias
+
+
+@dataclass
+class uInstr:
+    # What is moved over the data bus
+    dst: str = ""
+    src: str = "a"
+    # ALU settings
+    subtract: bool = False
+    update_flags: bool = False
+    # Other flags
+    count: bool = False  # increase PC
+    halt: bool = False
+
+
+class Mnemonic(enum.Enum):
+    NOP = 0x0
+    LDA = 0x1
+    ADD = 0x2
+    SUB = 0x3
+    STA = 0x4
+    LDI = 0x5
+    JMP = 0x6
+    JZ = 0x7
+    JC = 0x8
+    OUT = 0xE
+    HLT = 0xF
+
+
+OPCODES: dict[Mnemonic, list[uInstr]] = {
+    Mnemonic.NOP: [],
+    Mnemonic.LDA: [
+        uInstr(dst="memory_address", src="instruction"),
+        uInstr(dst="a", src="memory"),
+    ],
+    Mnemonic.ADD: [
+        uInstr(dst="memory_address", src="instruction"),
+        uInstr(dst="b", src="memory"),
+        uInstr(dst="a", src="alu", update_flags=True),
+    ],
+    Mnemonic.OUT: [
+        uInstr(dst="output", src="a"),
+    ],
+    Mnemonic.HLT: [uInstr(halt=True)],
+}
