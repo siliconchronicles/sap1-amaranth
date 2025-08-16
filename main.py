b22cde79a6a5d03b31192bbef059345bdf4b9dae
Daniel F Moisset <dfmoisset@gmail.com>
2025-08-16 23:20:06 +0100

    Simplify+Fix amaranth.cli main module

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from amaranth import ClockDomain, Module
 from amaranth.cli import main
 
 from beneater import BenEater
 
+FIBONACCI = [
+    0x51,  # LDI 1
+    0x4E,  # STA e
+    0x50,  # LDI 0
+    0xE0,  # OUT
+    0x2E,  # ADD e
+    0x4F,  # STA f
+    0x1E,  # LDA e
+    0x4D,  # STA d
+    0x1F,  # LDA f
+    0x4E,  # STA e
+    0x1D,  # LDA d
+    0x70,  # JC 0
+    0x63,  # JMP 3
+]
 
 if __name__ == "__main__":
-    sync = ClockDomain()
-
-    be8 = BenEater()
-
-    m = Module()
-    m.domains += sync
-    m.submodules += be8
-
-    main(m, ports=[sync.clk, sync.rst, be8.output_register.data_out])
+    be8 = BenEater(FIBONACCI)
+    main(be8, ports=[])
